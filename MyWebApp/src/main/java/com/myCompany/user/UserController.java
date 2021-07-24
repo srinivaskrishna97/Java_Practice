74d5e0a067124541412928d3897e8c61c702a1d5
srinivaskrishna97 <srinivaskrishna97@gmail.com>
2021-07-24 14:43:52 -0400

    Testing_CRUD_operations_Done

---
+++ MyWebApp/src/main/java/com/myCompany/user/UserController.java
@@ -0,0 +1,12 @@
+package com.myCompany.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+      @Autowired
+        private UserService service;
+
+
+}
--- MyWebApp/src/test/java/com/myCompany/UserRepositoryTests.java
+++ MyWebApp/src/test/java/com/myCompany/UserRepositoryTests.java
@@ -50,5 +50,22 @@ public class UserRepositoryTests {
         repo.save(user);
 
         User updatedUser = repo.findById(userId).get();
+        Assertions.assertThat(updatedUser.getPassword()).isEqualTo("hello200");
+   }
+   @Test
+    public void testGet(){
+        Integer userId = 2;
+        Optional<User> optionalUser = repo.findById(userId);
+        Assertions.assertThat(optionalUser).isPresent();
+       System.out.println(optionalUser.get());
+   }
+   @Test
+    public void testDelete(){
+        Integer userId = 2;
+        repo.deleteById(userId);
+
+        Optional<User> optionalUser = repo.findById(userId);
+        Assertions.assertThat(optionalUser).isNotPresent();
+
    }
 }
