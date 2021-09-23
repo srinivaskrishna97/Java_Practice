0f2dcfe61df8389cd37affa758bb9a71b6714e4a
srinivaskrishna97 <srinivaskrishna97@gmail.com>
2021-09-23 00:52:41 -0400

    Type_Casting

---
+++ CoreJavaPractice/src/com/company/TypeCasting.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+       double MyInt = 98.95;
+       int MyVariable = (int)MyInt;
+        System.out.println("MyInt:"+MyInt+" MyVariable:"+MyVariable);
+    }
+
+}
